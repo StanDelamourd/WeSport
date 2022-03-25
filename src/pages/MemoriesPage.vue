2f64730ee94620e4b81082c251ec858b6ae0a9a6
StanDelamourd <sdelamourd@gmail.com>
2022-03-25 18:13:27 +0100

    test push

--- src/pages/MemoriesPage.vue
+++ src/pages/MemoriesPage.vue
@@ -5,7 +5,7 @@
     <ion-footer>
             <ion-list>
                 <ion-item router-link="/memoriesPage/1">Trouver un groupe</ion-item>
-                <ion-item router-link="/landing">Login</ion-item>
+                <ion-item router-link="/landing">Logingf</ion-item>
                 <ion-item router-link="/memories/#">Profil</ion-item>
                 <ion-item router-link="/memories/#">Contact</ion-item>
             </ion-list>
