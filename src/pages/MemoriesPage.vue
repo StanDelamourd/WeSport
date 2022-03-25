fd6353c90e0e50c45bccbb39e9b8f2e47b9cddd1
StanDelamourd <sdelamourd@gmail.com>
2022-03-25 17:53:25 +0100

    adding firebase config

--- src/pages/MemoriesPage.vue
+++ src/pages/MemoriesPage.vue
@@ -1,22 +1,27 @@
 <template>
     <base-layout page-title="WeSport">
-    
+    <ion-searchbar placeholder="Recherche(ex. foot,volley,tennis...)"></ion-searchbar>
+    <ion-button>Filtre</ion-button>
+    <ion-footer>
             <ion-list>
                 <ion-item router-link="/memoriesPage/1">Trouver un groupe</ion-item>
                 <ion-item router-link="/landing">Login</ion-item>
                 <ion-item router-link="/memories/#">Profil</ion-item>
                 <ion-item router-link="/memories/#">Contact</ion-item>
             </ion-list>
+    </ion-footer>
      </base-layout>
 </template>
 
 <script>
-import {IonList, IonItem } from '@ionic/vue';
+import { IonList, IonItem, IonSearchbar, IonButton } from '@ionic/vue';
 
 export default{
     components: {
       IonList,
-      IonItem
+      IonItem,
+      IonSearchbar,
+      IonButton
     }
 }
 </script>
